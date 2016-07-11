3b035b620719286e7cb0d4a41d04844a87bf0558
Cuong Pham <Cuong Pham>
2016-07-11 17:31:39 +0700

    create apamama theme

---
+++ wp-content/themes/apamama/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Apamama</title>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>">
+	<?php wp_head() ?>
+</head>
+<body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="<?php echo get_template_directory_uri() ?>/js/bootstrap.min.js"></script>
+</body>
+</html>
