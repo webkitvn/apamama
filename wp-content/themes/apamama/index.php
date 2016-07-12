0f65f4c48be24161445a4b32c51891c1328f21a5
Cuong Pham <Cuong Pham>
2016-07-12 16:36:17 +0700

    code menu

--- wp-content/themes/apamama/index.php
+++ wp-content/themes/apamama/index.php
@@ -2,12 +2,35 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Apamama</title>
+	<script src="https://use.fontawesome.com/4c8792004d.js"></script>
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/helvetica-font.css">
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>">
 	<?php wp_head() ?>
 </head>
 <body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-12 col-md-12">
+				<nav>
+					<div class="menu">
+						<ul>
+							<li><a href="<?php echo bloginfo('home') ?>"><i class="fa fa-apple fa-2x" aria-hidden="true"></i></a></li>
+							<li><a href="#">Iphone</a></li>
+							<li><a href="#">Ipad</a></li>
+							<li><a href="#">Ốp lưng</a></li>
+							<li><a href="#">Bảo hành</a></li>
+							<li><a href="#">Tin tức</a></li>
+							<li><a href="#">Liên hệ</a></li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
+	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="<?php echo get_template_directory_uri() ?>/js/bootstrap.min.js"></script>
 </body>
